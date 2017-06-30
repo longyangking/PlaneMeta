846f1c081005a934828ed965df868985a7a45296
longyangking <longyang_123@yeah.net>
2017-06-30 20:35:35 +0800

    Basic structure for Tight binding

---
+++ PlaneMeta/TB/Hamiltonian.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Hamiltonian:
+    def __init__(self):
+        
+    def fit(self):
+        '''
+        Fit the LCAO Hamiltonian based on the Band structure calculated by n        numerical results
+        '''
+
+    def eigenvalues(self,k):
